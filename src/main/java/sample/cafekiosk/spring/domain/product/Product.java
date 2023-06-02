c810930341ecbfdbd8aebc2b4ceec78091d27740
khj1 <74335027+khj1@users.noreply.github.com>
2023-06-02 20:07:30 +0900

    [Spring/JPA 기반 테스트] - 테스트 없이 기능 구현

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,37 @@
+package sample.cafekiosk.spring.domain.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product extends BaseEntity {
+
+	@Id
+	@Column(name = "product_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String productNumber;
+
+	@Enumerated(EnumType.STRING)
+	private ProductType type;
+
+	@Enumerated(EnumType.STRING)
+	private ProductSellingStatus sellingStatus;
+
+	private String name;
+
+	private int price;
+
+}
