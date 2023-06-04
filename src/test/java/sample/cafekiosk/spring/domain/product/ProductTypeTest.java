13454ecb1eee94866bc8ddc05ba858498522b913
khj1 <74335027+khj1@users.noreply.github.com>
2023-06-04 21:02:58 +0900

    [더 나은 테스트를 위한 조언] - 테스트 환경의 독립성을 보장하자, 통제된 환경에서 테스트를 진행하자

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -4,24 +4,47 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * 테스트는 한 문단에 하나의 검증만 이루어지는 것이 좋다.
+ */
 class ProductTypeTest {
 
-	@DisplayName("타입이 병 음료 또는 베이커리라면 재고 타입에 해당한다.")
+	@DisplayName("타입이 병 라면 재고 타입에 해당한다.")
 	@Test
-	void containsStockType() {
+	void containsStockTypeWithBottle() {
 		// given
 		ProductType bottle = ProductType.BOTTLE;
-		ProductType bakery = ProductType.BAKERY;
-		ProductType handmade = ProductType.HANDMADE;
 
 		// when
 		boolean bottleResult = ProductType.containsStockType(bottle);
-		boolean bakeryResult = ProductType.containsStockType(bakery);
-		boolean handMadeResult = ProductType.containsStockType(handmade);
 
 		//then
 		Assertions.assertThat(bottleResult).isTrue();
+	}
+
+	@DisplayName("타입이 병 음료 또는 베이커리라면 재고 타입에 해당한다.")
+	@Test
+	void containsStockTypeWithBakery() {
+		// given
+		ProductType bakery = ProductType.BAKERY;
+
+		// when
+		boolean bakeryResult = ProductType.containsStockType(bakery);
+
+		//then
 		Assertions.assertThat(bakeryResult).isTrue();
+	}
+
+	@DisplayName("타입이 수제라면 재고 타입에 해당하지 않는다.")
+	@Test
+	void containsStockType() {
+		// given
+		ProductType handmade = ProductType.HANDMADE;
+
+		// when
+		boolean handMadeResult = ProductType.containsStockType(handmade);
+
+		//then
 		Assertions.assertThat(handMadeResult).isFalse();
 	}
 }
